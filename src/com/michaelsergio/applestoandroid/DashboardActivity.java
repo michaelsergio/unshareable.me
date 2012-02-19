8e9fb6226eb5b3e0305a10dcc772105932cb1ec6
Michael Sergio <mikeserg@gmail.com>
2012-02-18 19:44:11 -0800

    started some views

--- src/com/michaelsergio/applestoandroid/DashboardActivity.java
+++ src/com/michaelsergio/applestoandroid/DashboardActivity.java
@@ -1,13 +1,35 @@
 package com.michaelsergio.applestoandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class DashboardActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dashboard);
+		Button btnAnswer = (Button) findViewById(R.id.dashboard_answer);
+		Button btnAsk = (Button) findViewById(R.id.dashboard_ask);
+		
+		btnAnswer.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(DashboardActivity.this, QuestionResponsesActivity.class);
+				startActivity(i);
+			}
+		});
+
+		btnAsk.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(DashboardActivity.this, AskQuestionActivity.class);
+				startActivity(i);
+			}
+		});
 	}
 	
 }
